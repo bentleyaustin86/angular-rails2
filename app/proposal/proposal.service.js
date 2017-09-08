d7eebfeea1529a13fae17a20aa8a56fcede44f6e
bentleyaustin86 <bentleyaustin86@gmail.com>
2017-09-08 11:35:48 -0600

    updated styles and implemented proposal submit functionality as well as email capability for new proposals

--- app/proposal/proposal.service.js
+++ app/proposal/proposal.service.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/http");
 var Rx_1 = require("rxjs/Rx");
-var ProposalService = /** @class */ (function () {
+var ProposalService = (function () {
     function ProposalService(http) {
         this.http = http;
         this.proposalsUrl = 'http://localhost:3002/proposals';
@@ -24,6 +24,11 @@ var ProposalService = /** @class */ (function () {
     };
     ProposalService.prototype.getProposal = function (id) {
         return this.http.get(this.proposalsUrl + "/" + id + '.json');
+    };
+    ProposalService.prototype.createProposal = function (proposal) {
+        var headers = new http_1.Headers({ 'Content-Type': 'application/json' });
+        var options = new http_1.RequestOptions({ headers: headers });
+        return this.http.post(this.proposalsUrl, JSON.stringify(proposal), { headers: headers }).map(function (res) { return res.json(); });
     };
     ProposalService.prototype.handleError = function (error) {
         // In a real world app, we might use a remote logging infrastructure
